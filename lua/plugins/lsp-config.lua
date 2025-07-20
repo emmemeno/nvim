0b641664b1b58cff2eb909b90abce4a88d664287
Marco Piunti <me@marcopiunti.com>
2025-07-20 23:40:54 +0200

    shortcuts improvements

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -27,10 +27,13 @@ return {
                     allFeatures = true,
                 },
             })
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<leader>ck', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.definition, {})
             vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-            vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {})
+            vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, {})
+            vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, {})
+            vim.keymap.set('n', '<leader>cn', vim.diagnostic.goto_next, {})
+            vim.keymap.set('n', '<leader>cp', vim.diagnostic.goto_prev, {})
         end
     },
 }
