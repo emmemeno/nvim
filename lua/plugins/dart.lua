3befc2aa070e6003110d2eb3cd740581b7f7c1b9
emmemeno <pompelmo@gmail.com>
2025-09-04 07:27:39 +0200

    mark list reversed order

--- lua/plugins/dart.lua
+++ lua/plugins/dart.lua
@@ -1,4 +1,15 @@
 return {
     'iofq/dart.nvim',
-    opts = {}
+    opts = {
+        tabline = {
+            icons = false,
+            order = function(config)
+              local order = {}
+              for i, key in ipairs(vim.list_extend(vim.deepcopy(config.marklist), config.buflist)) do
+                order[key] = i
+              end
+              return order
+            end,
+        }
+    }
 }
